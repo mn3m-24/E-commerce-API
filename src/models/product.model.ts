adc65581597a39d7454ef4dcaf91f7bf0b8810d6
abdalmonem <112807908+mn3m-24@users.noreply.github.com>
2025-09-28 14:52:58 +0300

    feat: modify the product model schema & add interface

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -1,12 +1,24 @@
-import { Schema, model } from "mongoose";
+import { Schema, model, type ObjectId, type Document } from "mongoose";
 
-const productSchema = new Schema(
+export interface IProduct extends Document {
+    name: string;
+    price: number;
+    stock: number;
+    category: ObjectId;
+}
+
+const productSchema = new Schema<IProduct>(
     {
         name: { type: String, required: true, unique: true },
         price: { type: Number, required: true },
-        inventory: { type: Number, required: true },
+        stock: { type: Number, default: 0 },
+        category: {
+            type: Schema.Types.ObjectId,
+            ref: "Category",
+            required: true,
+        },
     },
     { timestamps: true },
 );
 
-export default model("Product", productSchema);
+export default model<IProduct>("Product", productSchema);
