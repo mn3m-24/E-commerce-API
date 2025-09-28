e1e4c0e075a44b69d969e58a040a48cf9b4f43f4
abdalmonem <112807908+mn3m-24@users.noreply.github.com>
2025-09-28 15:00:37 +0300

    refactor: authentication middleware

--- src/middlewares/isAuthenticated.ts
+++ src/middlewares/isAuthenticated.ts
@@ -1,16 +1,18 @@
-import type { RequestHandler } from "express";
-import type { JwtPayload } from "jsonwebtoken";
+import type { Request, Response, NextFunction } from "express";
 import { verifyAccessToken } from "../utils/jwt.utils.ts";
 
-export const isAuthenticated: RequestHandler = (req, res, next) => {
+export default (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers["authorization"];
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
         return res.status(401).json({ error: "Unauthorized" });
     }
     const token = authHeader.split(" ")[1];
     try {
-        const decoded = verifyAccessToken(token as string) as JwtPayload;
-        req.userId = decoded.userId;
+        const decoded = verifyAccessToken(token!) as {
+            sub: string;
+            role: "customer" | "admin";
+        };
+        req.user = decoded;
         return next();
     } catch (err) {
         console.error(err);
