68d3db38c4dc6cccec7df1fdb43dfaa376ace892
abdalmonem <112807908+mn3m-24@users.noreply.github.com>
2025-10-03 09:56:48 +0300

    feat: enable different params instead of hardcoded id

--- src/middlewares/isObjectId.ts
+++ src/middlewares/isObjectId.ts
@@ -1,7 +1,8 @@
 import type { Request, Response, NextFunction } from "express";
 import { isValidObjectId } from "mongoose";
 
-export default (req: Request, res: Response, next: NextFunction) =>
-    isValidObjectId(req.params.id)
-        ? next()
-        : res.status(400).json({ message: "Invalid Id path param" });
+export default (paramName: string = "id") =>
+    (req: Request, res: Response, next: NextFunction) =>
+        isValidObjectId(req.params[paramName])
+            ? next()
+            : res.status(400).json({ message: `Invalid ${paramName} format` });
