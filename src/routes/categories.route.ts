bc6a5c1178b6bd6869c0f7e7d55c0f49fca34d05
abdalmonem <112807908+mn3m-24@users.noreply.github.com>
2025-10-29 05:42:06 +0300

    feat(category): add authorization & objectId validation

--- src/routes/categories.route.ts
+++ src/routes/categories.route.ts
@@ -8,21 +8,30 @@ import {
     deleteCategory,
     getCategoryProducts,
 } from "../controllers/categories.controller.ts";
+import validate from "../middlewares/validateSchema.ts";
 import authorizeRole from "../middlewares/authorizeRole.ts";
+import isObjectId from "../middlewares/isObjectId.ts";
+import { categoryBodySchema } from "../validation/categories.schema.ts";
 
 const categoriesRouter: Router = Router().use(isAuthenticated);
 
 categoriesRouter
     .route("/")
     .get(getAllCategories)
-    .post(authorizeRole("admin"), createCategory);
+    .post(
+        authorizeRole("admin"),
+        validate(categoryBodySchema, "body"),
+        createCategory,
+    );
+
 categoriesRouter
     .route("/:id")
+    .all(isObjectId())
     .get(getCategory)
     .all(authorizeRole("admin"))
-    .put(updateCategory)
+    .put(validate(categoryBodySchema.partial(), "body"), updateCategory)
     .delete(deleteCategory);
 
-categoriesRouter.route("/:id/products").get(getCategoryProducts);
+categoriesRouter.get("/:id/products", isObjectId(), getCategoryProducts);
 
 export default categoriesRouter;
