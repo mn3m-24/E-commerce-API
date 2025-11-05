d35c213628820e8f83c5a0c1103099e06819eeef
abdalmonem <112807908+mn3m-24@users.noreply.github.com>
2025-11-05 12:38:38 +0200

    feat(models): add validation & indexes to orders model

--- src/models/order.model.ts
+++ src/models/order.model.ts
@@ -1,18 +1,33 @@
 import { Schema, model } from "mongoose";
 import type { IOrder, IOrderItem } from "../types/orders.types.ts";
 
-const orderItemSchema = new Schema<IOrderItem>({
-    product: { type: Schema.Types.ObjectId, ref: "Product", required: true },
-    quantity: { type: Number, required: true },
-    priceAtPurchase: { type: Number, required: true },
-});
+const orderItemSchema = new Schema<IOrderItem>(
+    {
+        product: {
+            type: Schema.Types.ObjectId,
+            ref: "Product",
+            required: true,
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            min: [1, "Quantity must be at least 1"],
+        },
+        priceAtPurchase: {
+            type: Number,
+            required: true,
+            min: [0, "Price cannot be negative"],
+        },
+    },
+    { _id: false },
+);
 
 const orderSchema = new Schema<IOrder>(
     {
         userId: { type: Schema.Types.ObjectId, ref: "User" },
         items: [orderItemSchema],
         totalPrice: { type: Number, required: true },
-        shippingAddress: String,
+        shippingAddress: { type: String, required: true },
         status: {
             type: String,
             enum: ["pending", "paid", "shipped", "delivered", "cancelled"],
@@ -22,5 +37,6 @@ const orderSchema = new Schema<IOrder>(
     },
     { timestamps: true },
 );
+orderSchema.index({ status: "text", shippingAddress: "text" });
 
 export default model<IOrder>("Order", orderSchema);
